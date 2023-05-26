0aa3c36e88246424f309992ad50b16caf8f664f2
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-25 21:45:05 -0600

    Add display & buttons, and functions to fill and clear the display

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let num1 = "";
 let operator = "";
 let num2 = "";
+let display_value = "";
+let display = document.querySelector(".display");
+
+document.querySelectorAll(".number").forEach(number => 
+    number.addEventListener("click", fillDisplay));
+document.querySelector("#clear").addEventListener("click", clear);
+
+function fillDisplay(e) {
+    console.log(e.target.textContent); //delete this
+    display_value += e.target.textContent;
+    display.textContent = display_value;
+}
+
+function clear(e) {
+    display_value = "";
+    display.textContent = "0";
+}
 
 function operate(num1, operator, num2) {
     return operator(num1, num2);
