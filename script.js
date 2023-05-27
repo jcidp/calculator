fb9df0d9f70fb9770796258f5171c029bd59b41b
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-27 13:37:33 -0600

    Make the calculator work with a function for operators

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let num1 = "";
+let num1;
 let operator = "";
-let num2 = "";
 let display_value = "";
 let display = document.querySelector(".display");
 
 document.querySelectorAll(".number").forEach(number => 
     number.addEventListener("click", fillDisplay));
 document.querySelector("#clear").addEventListener("click", clear);
+document.querySelectorAll(".operator").forEach(operator =>
+    operator.addEventListener("click", applyOperator));
+
 
 function fillDisplay(e) {
     console.log(e.target.textContent); //delete this
@@ -14,13 +16,32 @@ function fillDisplay(e) {
     display.textContent = display_value;
 }
 
+function applyOperator(e) {
+    if (operator) {
+        let result = operate(num1, operator, Number(display_value));
+        display_value = result.toString();
+        display.textContent = display_value;
+    }
+    num1 = Number(display_value);
+    operator = e.target.textContent === "=" ? "" : e.target.textContent;
+    if (operator !== "") display_value = "";
+}
+
 function clear(e) {
     display_value = "";
     display.textContent = "0";
+    operator = "";
 }
 
 function operate(num1, operator, num2) {
-    return operator(num1, num2);
+    operator_functions = {
+        "+": add,
+        "-": subtract,
+        "x": multiply,
+        "÷": divide,
+        "=": equals
+    }
+    return operator_functions[operator](num1, num2);
 }
 
 function add(a, b) {
@@ -37,4 +58,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function equals(a, b) {
+    return
 }
