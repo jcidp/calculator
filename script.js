33fa339cab916932c3d559e0f2276b7fb065a667
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-28 11:39:22 -0600

    Handle user keypresses as input

--- script.js
+++ script.js
@@ -11,19 +11,34 @@ document.querySelector("#clear").addEventListener("click", clear);
 document.querySelectorAll(".operator").forEach(operator =>
     operator.addEventListener("click", applyOperator));
 document.querySelector("#backspace").addEventListener("click", backspace);
-//document.querySelector("#dot").addEventListener("click", addDecimalPoint);
 
-// Bugs to fix: Writing 0.0001
+document.addEventListener("keyup", keypressHandler);
+
+function keypressHandler(e) {
+    console.log(`Key: ${e.key} Code: ${e.code}`);
+    if (e.key === "Backspace") backspace();
+    if ("0123456789.".includes(e.key)) fillDisplay(e.key);
+    if ("+-*/=".includes(e.key)) applyOperator(e.key);
+    if (e.key === "Enter") applyOperator("=");
+    if (e.key === "Escape" || e.key === "Delete") clear();
+}
+
+function test(e) {
+    console.log(`Target: ${e.target.textContent}`)
+}
 
 function fillDisplay(e) {
     error.style.visibility = "hidden";
-    console.log(e.target.textContent); //delete this
-    if (display.textContent === "0" && (e.target.textContent !== "." || !display_value.includes("."))) {
-        display_value = e.target.textContent === "." ? "0." : e.target.textContent;
+    let input = e.target ? e.target.textContent : e;
+    if (input === "." && display_value.includes(".")) return;
+    console.log(input); //delete this
+    if (display.textContent === "0" && (input !== "." || !display_value.includes("."))) {
+        display_value = input === "." ? "0." : input;
         display.textContent = display_value;
         return;
     }
-    display_value += e.target.textContent === "." ? "0." : e.target.textContent;
+    display_value += input !== "." ? input : 
+    display_value === "" ? "0." : ".";
     display.textContent = display_value;
     if (display_value.includes(".")) dot.disabled = true;
     else dot.disabled = false;
@@ -46,8 +61,9 @@ function applyOperator(e) {
         display_value = (Math.round(result * 1000) / 1000).toString();
         display.textContent = display_value;
     }
+    let input = e.target ? e.target.textContent : e;
     num1 = Number(display_value);
-    operator = e.target.textContent === "=" ? "" : e.target.textContent;
+    operator = input === "=" ? "" : input;
     if (operator !== "") display_value = "";
     if (display_value.includes(".")) dot.disabled = true;
     else dot.disabled = false;
@@ -65,8 +81,8 @@ function operate(num1, operator, num2) {
     operator_functions = {
         "+": add,
         "-": subtract,
-        "x": multiply,
-        "÷": divide,
+        "*": multiply,
+        "/": divide,
         "=": equals
     }
     return operator_functions[operator](num1, num2);
